4371559535bba00599068b6476fa9d0cf74cd774
Daniel Nelson <lukannelson@gmail.com>
2024-08-21 23:04:22 -0400

    fixed responsiveness

--- src/components/GalleryCard.jsx
+++ src/components/GalleryCard.jsx
@@ -67,7 +67,7 @@ const GalleryCard = ({ product, currentPage, showViolentContent }) => {
       setCurrentImageIndex((prevIndex) =>
         prevIndex === product.image.length - 1 ? 0 : prevIndex + 1
       );
-    }, 5); // Change the duration here for the image transition
+    }, 7); // Change the duration here for the image transition
     return () => clearInterval(interval);
   }, [product.image.length]);
 
